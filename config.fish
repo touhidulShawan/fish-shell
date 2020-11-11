16f69d94160f69f51baa5717ff7a24c97f7d2b58
Touhidul Shawan <touhidulshawan@gmail.com>
2020-11-11 17:44:56 +0600

    add nvm wrapper

--- config.fish
+++ config.fish
@@ -7,16 +7,6 @@
 # |_|    |_____|_____/|_|  |_|
 # -----------------------------
 
-
-# Path to Oh My Fish install.
-set -q XDG_DATA_HOME
-  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
-  or set -gx OMF_PATH "$HOME/.local/share/omf"
-
-# Load Oh My Fish configuration.
-#source $OMF_PATH/init.fish
-
-
 set -x EDITOR nvim
 
 # load starship prompt
